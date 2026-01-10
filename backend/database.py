fa7146a1d3b7a491501edef33613be2dd2ea7429
JayShinde1 <jayshinde4343@gmail.com>
2026-01-11 04:39:26 +0530

    Make database configurable via DATABASE_URL

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from typing import Annotated
 from fastapi import Depends
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./books.db'
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./books.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 
-Session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+Session_local = sessionmaker(
+    autoflush = False,
+    autocommit = False,
+    bind = engine
+)
 
 def get_db():
     db = Session_local()
